3e3016e10f588ba5277373d24c9708d70ecc967f
Riccardo Melioli <richkmeli@gmail.com>
2019-11-17 16:16:55 +0100

    completed tokenManager and its jUnit tests.

--- src/main/java/it/richkmeli/jframework/crypto/controller/token/TokenManager.java
+++ src/main/java/it/richkmeli/jframework/crypto/controller/token/TokenManager.java
@@ -1,6 +1,7 @@
 package it.richkmeli.jframework.crypto.controller.token;
 
 import it.richkmeli.jframework.crypto.algorithm.SHA256;
+import it.richkmeli.jframework.crypto.util.RandomStringGenerator;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -15,16 +16,17 @@ public class TokenManager {
 
     public static String generateTemporized(String value, int minutesOfValidity) {
         // eg: abcdefgh
-        String salt = "________";//RandomStringGenerator.generateAlphanumericString(8);
+        String salt = RandomStringGenerator.generateAlphanumericString(9);
 
         long currentTimeMillis = System.currentTimeMillis();
         long currentTimeSeconds = currentTimeMillis / 1000;
         long currentTimeMinutes = currentTimeSeconds / 60;
-        System.out.println("gen: " + salt + " " + value + " " + currentTimeMinutes * minutesOfValidity);
+        //System.out.println("gen: " + salt + " " + value + " " + (currentTimeMinutes * minutesOfValidity));
         // eg: 0000000000000000000000000000000000000000000000000000000000000000
-        String hashed = SHA256.hash(salt + value + currentTimeMinutes * minutesOfValidity);
+        String hashed = SHA256.hash(salt + value + (currentTimeMinutes * minutesOfValidity));
 
-        int interval = hashed.length() /*64*/ / salt.length();
+        int interval = ((hashed.length() + salt.length()) / salt.length());
+        //System.out.println("interval: " + interval);
 
         String out = insertSaltIntoHashed(salt, hashed, interval);
 
@@ -36,24 +38,26 @@ public class TokenManager {
         return verifyTemporized(token, value, 0);
     }
 
+    // minutesOfValidity has to be aligned to the value set in generateTemporized
     public static boolean verifyTemporized(String token, String value, int minutesOfValidity) {
+        int calculatedInterval = token.length() / (token.length() - 64);
+        //System.out.println("calculatedInterval: " + calculatedInterval);
+
         List<String> list = extractSaltHashedFromToken(token, 8);
         String salt = list.get(0);
         String hashed = list.get(1);
-        System.out.println("hashed: " + hashed);
+        //System.out.println("hashed: " + hashed);
 
         long currentTimeMillis = System.currentTimeMillis();
         long currentTimeSeconds = currentTimeMillis / 1000;
         long currentTimeMinutes = currentTimeSeconds / 60;
 
-        System.out.println("ver: " + salt + " " + value + " " + currentTimeMinutes * minutesOfValidity);
-        String calculatedHash = SHA256.hash(salt + value + currentTimeMinutes * minutesOfValidity);
-        System.out.println("calculatedHash: " + calculatedHash);
-
+        //System.out.println("ver: " + salt + " " + value + " " + (currentTimeMinutes * minutesOfValidity));
+        String calculatedHash = SHA256.hash(salt + value + (currentTimeMinutes * minutesOfValidity));
+        //System.out.println("calculatedHash: " + calculatedHash);
 
         String calculatedHashWithSalt = insertSaltIntoHashed(salt, calculatedHash, 8);
-        System.out.println("calculatedHashWithSalt: " + calculatedHashWithSalt);
-
+        //System.out.println("calculatedHashWithSalt: " + calculatedHashWithSalt);
 
         if (hashed.equalsIgnoreCase(calculatedHash)) {
             return true;
@@ -66,9 +70,9 @@ public class TokenManager {
     private static String insertSaltIntoHashed(String salt, String hashed, int interval) {
         StringBuilder out = new StringBuilder();
         for (int i = 0, iS = 0; i < hashed.length(); ++i) {
-            if (((i + iS) % interval) == 7) {
+            if (((i + iS) % interval) == (interval - 1)) {
                 // [0,1,2,3]
-                out.append(salt.charAt(i / interval));
+                out.append(salt.charAt((i + iS) / interval));
                 iS++;
             }
             out.append(hashed.charAt(i));
@@ -80,14 +84,13 @@ public class TokenManager {
     private static List<String> extractSaltHashedFromToken(String token, int interval) {
         StringBuilder salt = new StringBuilder();
         StringBuilder hashed = new StringBuilder();
-        for (int i = 0, iS = 0; i < token.length(); ++i) {
-            if (((i + iS) % interval) == 7) {
+        for (int i = 0; i < token.length(); ++i) {
+            if ((i % interval) == (interval - 1)) {
                 salt.append(token.charAt(i));
-                iS++;
-                System.out.println("s" + i + ": " + token.charAt(i));
+                //System.out.println("s" + i + ": " + token.charAt(i));
             } else {
                 hashed.append(token.charAt(i));
-                System.out.println("h" + i + ": " + token.charAt(i));
+                //System.out.println("h" + i + ": " + token.charAt(i));
             }
         }
         return new ArrayList<>(Arrays.asList(salt.toString(), hashed.toString()));
--- src/test/java/it/richkmeli/jframework/crypto/controller/token/TokenManagerTest.java
+++ src/test/java/it/richkmeli/jframework/crypto/controller/token/TokenManagerTest.java
@@ -2,24 +2,40 @@ package it.richkmeli.jframework.crypto.controller.token;
 
 import org.junit.Test;
 
+import static it.richkmeli.jframework.crypto.algorithm.algorithmTestUtil.genString;
+import static it.richkmeli.jframework.crypto.algorithm.algorithmTestUtil.plainTextLengths;
+
 public class TokenManagerTest {
 
     @Test
     public void generate_verify() {
-        String token = TokenManager.generate("test");
+        for (int i : plainTextLengths) {
+            String value = genString(i);
+            String token = TokenManager.generate(value);
 
-        System.out.println(token);
+            //System.out.println("TEST: " + token);
 
-        assert TokenManager.verify(token, "test");
+            assert TokenManager.verify(token, value);
+        }
     }
 
     @Test
     public void generate_verify_temporized() {
-        String token = TokenManager.generateTemporized("test", 2);
+        for (int i : plainTextLengths) {
+            String value = genString(i);
+            for (int minOfVal = 0; i < 5; minOfVal++) {
+                String token = TokenManager.generateTemporized(value, minOfVal);
 
-        System.out.println(token);
+                //System.out.println("TEST temp: " + token);
 
-        assert TokenManager.verifyTemporized(token, "test", 2);
+                assert TokenManager.verifyTemporized(token, value, minOfVal);
+            }
+        }
+
+        // expired token
+        String token = "9f12cce3aaa6de6C75362a4E8ee74b2k70a9e84U89b9de8Y5b7faddnf5fe7c9Id9072e5xc"; /*17 nov 19 - 12:45*/
+        //System.out.println("TEST temp: " + token);
+        assert !TokenManager.verifyTemporized(token, "expiredToken", 1);
     }
 
 }
